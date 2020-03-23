33d62111991104e8f4eed759da27f84678875b67
Bealby <mgbealby@gmail.com>
2020-03-23 15:13:53 +0000

    Added content for .js script

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,5 @@
+// Bad practice to run jQuery before page fully loaded. Use ready function. 
 $(document).ready(function() {
-    
+    $("h2").css("text-decoration", "underline"); // Underlines all <h2> elements
+    $("ul").css("border", "solid 1px #ccc"); // Adds a border to all lists
 });
